0eca0a60761a5316a26f21c0185884e8bb232cd6
Tilova Shahrin <46762829+tilovashahrin@users.noreply.github.com>
2024-04-21 13:51:41 -0400

    peak time histogram

--- Notebooks/main.py
+++ Notebooks/main.py
@@ -18,8 +18,7 @@ def load_data(path, num_rows):
 # load data 1755214
 df = load_data("../data/parking_coord_5_rows.csv", 5)
 st.write('Here are the first few rows of Toronto\'s Parking Ticket data from 2016 to 2022')
-st.dataframe(df.head())
-df_coord = df[df['latitude'] != 0]
+st.dataframe(df)
 #######################################################################################################################################
 ### Describing the dataset
 st.subheader("City of Toronto's Parking Enforcement")
@@ -41,7 +40,7 @@ st.subheader("Exploratory Data Analysis")
 #######################################################################################################################################
 ### Create a map
 st.write("All Parking Tickets in Toronto")
-st.map(df_coord, size=7, color="#639cd9")   
+st.map(df, size=7, color="#639cd9")   
 
 #######################################################################################################################################
 ### Create EDAs
@@ -96,28 +95,28 @@ chart3 = alt.Chart(heatmap_lat_lon).mark_rect().encode(
 )
 st.altair_chart(chart3, theme='streamlit', use_container_width=True)
 
-#chart 4
-df['datetime_of_infraction'] = pd.to_datetime(df['datetime_of_infraction'])
+# #chart 4
+# df['datetime_of_infraction'] = pd.to_datetime(df['datetime_of_infraction'])
 
-# Filter data for the year 2022
-df_2022 = df[df['datetime_of_infraction'].dt.year == 2022]
-df_2022['hour'] = df['datetime_of_infraction'].dt.hour
+# # Filter data for the year 2022
+# df_2022 = df[df['datetime_of_infraction'].dt.year == 2022]
+# df_2022['hour'] = df['datetime_of_infraction'].dt.hour
 
-# Define hour categories
-hour_categories = {
-    0: '00:00', 1: '01:00', 2: '02:00', 3: '03:00',
-    4: '04:00', 5: '05:00', 6: '06:00', 7: '07:00',
-    8: '08:00', 9: '09:00', 10: '10:00', 11: '11:00',
-    12: '12:00', 13: '13:00', 14: '14:00', 15: '15:00',
-    16: '16:00', 17: '17:00', 18: '18:00', 19: '19:00',
-    20: '20:00', 21: '21:00', 22: '22:00', 23: '23:00'
-}
+# # Define hour categories
+# hour_categories = {
+#     0: '00:00', 1: '01:00', 2: '02:00', 3: '03:00',
+#     4: '04:00', 5: '05:00', 6: '06:00', 7: '07:00',
+#     8: '08:00', 9: '09:00', 10: '10:00', 11: '11:00',
+#     12: '12:00', 13: '13:00', 14: '14:00', 15: '15:00',
+#     16: '16:00', 17: '17:00', 18: '18:00', 19: '19:00',
+#     20: '20:00', 21: '21:00', 22: '22:00', 23: '23:00'
+# }
 
-# Categorize hours into time ranges
-df_2022['hour_category'] = df_2022['hour'].map(hour_categories)
+# # Categorize hours into time ranges
+# df_2022['hour_category'] = df_2022['hour'].map(hour_categories)
 
-# Group by hour category and count infractions
-infraction_hour = df_2022.groupby('hour_category').size().reset_index(name='count')
+# # Group by hour category and count infractions
+infraction_hour = load_data("../data/hour_peak_histogram.csv", 24)
 
 chart4 = alt.Chart(infraction_hour).mark_bar(color="#639cd9").encode(
     x=alt.X('hour_category:O', title='Hour of Day',  axis=alt.Axis(labelAngle=-45)),
